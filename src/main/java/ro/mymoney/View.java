a90fe12b3ba92ebf28c685ea96c1d93591ac8cb8
vculea <vculea@sdl.com>
2022-05-16 08:47:42 +0300

    improvement

--- src/main/java/ro/mymoney/View.java
+++ src/main/java/ro/mymoney/View.java
@@ -1,32 +1,36 @@
 package ro.mymoney;
 
+import com.sdl.selenium.extjs6.button.Button;
 import com.sdl.selenium.extjs6.form.ComboBox;
 import com.sdl.selenium.extjs6.form.DateField;
 import com.sdl.selenium.extjs6.form.TextField;
-import com.sdl.selenium.extjs6.window.Window;
-import com.sdl.selenium.extjs6.button.Button;
 import com.sdl.selenium.extjs6.grid.Grid;
+import com.sdl.selenium.extjs6.window.Window;
+import com.sdl.selenium.web.utils.RetryUtils;
 import com.sdl.selenium.web.utils.Utils;
+import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
+@Getter
 public class View {
     private static final Logger LOGGER = LoggerFactory.getLogger(View.class);
 
     public View() {
     }
 
-    private final Grid grid = new Grid();
-    private Button add = new Button(grid, "Add Transaction");
-    private Window addNew = new Window("Add New");
-    private TextField name = new TextField(addNew, "Name:");
-    private ComboBox category = new ComboBox(addNew, "Category:");
-    private ComboBox subCategory = new ComboBox(addNew, "Subcategory:");
-    private DateField dateField = new DateField(addNew, "Data:");
-    private TextField sumaField = new TextField(addNew, "Price:");
-    private Button saveButton = new Button(addNew, "Save");
+    private final Grid grid = new Grid().setExcludeClasses("x-grid-locked", "x-tree-panel", "x-grid-header-hidden");
+    private final Button add = new Button(grid, "Add Transaction");
+    private final Window addNew = new Window("Add New");
+    private final TextField name = new TextField(addNew, "Name:");
+    private final ComboBox category = new ComboBox(addNew, "Category:").setLabelPosition("//following-sibling::*//");
+    private final ComboBox subCategory = new ComboBox(addNew, "Subcategory:").setLabelPosition("//following-sibling::*//");
+    private final DateField dateField = new DateField(addNew, "Data:");
+    private final TextField sumaField = new TextField(addNew, "Price:");
+    private final Button saveButton = new Button(addNew, "Save");
+    private final Button leftButton = new Button().setIconCls("fa-chevron-left");
 
     public boolean addInsert(String denum, String cat, String sub, String sum) {
         add.ready(Duration.ofSeconds(10));
@@ -46,8 +50,8 @@ public class View {
         add.click();
         name.setValue(denum);
         category.select(cat, Duration.ofSeconds(1));
-        subCategory.select(sub, Duration.ofSeconds(1));
-        dateField.select(data, "dd/MM/yyyy");
+        RetryUtils.retry(2, () -> subCategory.select(sub, Duration.ofSeconds(1)));
+        dateField.select(data, "dd.MM.yyyy");
         sumaField.setValue(sum);
         return saveButton.click();
     }
--- src/test/java/ro/mymoney/MyMoneySteps.java
+++ src/test/java/ro/mymoney/MyMoneySteps.java
@@ -1,6 +1,9 @@
 package ro.mymoney;
 
 import com.google.common.base.Strings;
+import com.sdl.selenium.extjs6.button.Button;
+import com.sdl.selenium.extjs6.grid.Cell;
+import com.sdl.selenium.extjs6.grid.Row;
 import com.sdl.selenium.web.utils.Utils;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
@@ -13,16 +16,17 @@ import org.apache.commons.csv.CSVRecord;
 import org.fasttrackit.util.TestBase;
 
 import java.io.BufferedReader;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Locale;
 
 @Slf4j
 public class MyMoneySteps extends TestBase {
@@ -41,35 +45,82 @@ public class MyMoneySteps extends TestBase {
 
     @And("I read csv file and insert date")
     public void iReadCsvFileAndInsertDate() {
+        List<Item> notFoundSubCategory = new ArrayList<>();
+        List<Item> isAlreadyExist = new ArrayList<>();
         List<Item> items = readCSV("C:\\Users\\vculea\\Desktop\\BT\\Aprilie.csv");
-        for (Item item : items) {
-            String subCategory = getSubCategory(item.getName());
-            if (Strings.isNullOrEmpty(subCategory)) {
-                log.info(item.toString());
-            } else {
-                view.addInsert(item.getName(), "Cheltuieli", subCategory, item.getDate(), item.getSum().replace(",", "."));
+        LocalDate d = LocalDate.parse(items.get(0).getDate(), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String monthAndYear = d.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH) + " " + d.getYear();
+        Button button = new Button(null, monthAndYear);
+        boolean inCorrectView = false;
+        if (!button.ready()) {
+            view.getLeftButton().click();
+            inCorrectView = button.ready();
+        }
+        if (inCorrectView) {
+            view.getGrid().ready(true);
+            for (Item item : items) {
+                view.getGrid().scrollTop();
+                LocalDate datetime = LocalDate.parse(item.getDate(), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+                String date = datetime.format(DateTimeFormatter.ofPattern("dd-MMM-yyyy", Locale.ENGLISH));
+                String sum = getCorrectValue(item.getSum());
+                Row row = view.getGrid().getRow(new Cell(4, date), new Cell(5, sum));
+                if (!row.waitToRender(Duration.ofSeconds(2), false) && !row.scrollTo()) {
+                    String subCategory = getSubCategory(item.getName());
+                    if (Strings.isNullOrEmpty(subCategory)) {
+                        notFoundSubCategory.add(item);
+                    } else {
+                        view.addInsert(subCategory, "Cheltuieli", subCategory, item.getDate(), item.getSum());
+                    }
+                } else {
+                    isAlreadyExist.add(item);
+                }
             }
         }
+        log.info("Nu s-a putut gasi subcategori pentru:");
+        notFoundSubCategory.forEach(i -> log.info(i.toString()));
+        log.info("Deja erau adaugate:");
+        isAlreadyExist.forEach(i -> log.info(i.toString()));
+        log.info("Diferenta: {}", items.size() - isAlreadyExist.size() - notFoundSubCategory.size());
+        Utils.sleep(1);
+    }
+
+    private String getCorrectValue(String sum) {
+        double s = Double.parseDouble(sum);
+        DecimalFormat df = new DecimalFormat("#.#");
+        return df.format(s).replace(",", ".");
+    }
+
+    public static void main(String[] args) {
+        MyMoneySteps d = new MyMoneySteps();
+        String correctValue = d.getCorrectValue("2.34");
+        String correctValue1 = d.getCorrectValue("2.56");
         Utils.sleep(1);
     }
 
     private String getSubCategory(String name) {
         String subCategory = null;
-        if (name.contains("Lidl") || name.contains("AUCHAN") || name.contains("PENNY") || name.contains("KAUFLAND") ||
-                name.contains("MEGAIMAGE") || name.contains("BONAS") || name.contains("LA VESTAR")
+        if (name.contains("Lidl") || name.contains("LIDL") || name.contains("AUCHAN") || name.contains("PENNY") || name.contains("KAUFLAND") ||
+                name.contains("MEGAIMAGE") || name.contains("BONAS") || name.contains("LA VESTAR") || name.contains("PROFI") ||
+                name.contains("CICMAR") || name.contains("VARGA") || name.contains("BUCURCRISS")
         ) {
             subCategory = "Produse alimentare";
         } else if (name.contains("HORNBACH") || name.contains("LEROY MERLIN")) {
             subCategory = "Casa";
-        } else if (name.contains("ZARA") || name.contains("H&M") || name.contains("PEPCO")) {
+        } else if (name.contains("ZARA") || name.contains("H&M") || name.contains("PEPCO") || name.contains("ORGANIZATIA CRESTINA")) {
             subCategory = "Haine";
         } else if (name.contains("OMV")) {
             subCategory = "Masina";
+        } else if (name.contains("EXCELLENTE SOURCE") || name.contains("EUROTRANS SRL")) {
+            subCategory = "Alte Cheltuieli";
         } else if (name.contains("REMEDIUM")) {
             subCategory = "Medicamente";
+        } else if (name.contains("ABURIDO SRL")) {
+            subCategory = "Igiena";
+        } else if (name.contains("ANDY EVENTS")) {
+            subCategory = "Cadouri";
         } else if (name.contains("CTP") || name.contains("tpark.ro")) {
             subCategory = "Transport";
-        } else if (name.contains("LEMNUL VERDE") || name.contains("ASI BAKLAVA")) {
+        } else if (name.contains("LEMNUL VERDE") || name.contains("ASI BAKLAVA") || name.contains("MOLDOVAN CARMANGERIE")) {
             subCategory = "Restaurant";
         }
         return subCategory;
@@ -87,7 +138,7 @@ public class MyMoneySteps extends TestBase {
             if (val.contains("2022")) {
                 List<String> values = record.toList();
                 if ("Decontat".equals(values.get(2))) {
-                    list.add(new Item(values.get(0), values.get(2), values.get(3), values.get(5)));
+                    list.add(new Item(values.get(0), values.get(2), values.get(3), values.get(5).replace(",", ".")));
                 }
             }
         }
